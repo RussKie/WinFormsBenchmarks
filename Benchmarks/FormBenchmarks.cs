a7557f04e6604f9a85c7af5a0a9e0e55a104b7b5
Igor Velikorossov <igor.velikorossov@microsoft.com>
2020-01-06 17:41:37 +1100

    Initial

---
+++ Benchmarks/FormBenchmarks.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Windows.Forms;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class FormBenchmarks
+    {
+        [Benchmark]
+        public bool CreateAndShowForm()
+        {
+            using (var form = new Form())
+            {
+                form.Show();
+                form.Close();
+            }
+            return true;
+        }
+    }
+}
---
+++ Benchmarks/Program.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.CsProj;
+
+namespace Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = DefaultConfig.Instance
+                .With(Job.Default.With(CsProjClassicNetToolchain.Net472))
+                .With(Job.Default.With(CsProjCoreToolchain.NetCoreApp31))
+                //.With(Job.Default.With(CsProjCoreToolchain.From(new NetCoreAppSettings("netcoreapp5.0", null, ".NET Core 5.0"))))
+                ;
+
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
+        }
+    }
+}
