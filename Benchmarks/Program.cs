c5834615647e392d63f0a7a82ed92cdc158f6dec
Adam Sitnik <adam.sitnik@gmail.com>
2021-02-08 13:42:37 +0100

    prefer command line args over explicit Job definition in code

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,22 +1,14 @@
-﻿using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Toolchains.CsProj;
-using BenchmarkDotNet.Toolchains.DotNetCli;
+﻿using BenchmarkDotNet.Running;
 
 namespace Benchmarks
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var config = DefaultConfig.Instance
-                .AddJob(Job.Default.WithToolchain(CsProjClassicNetToolchain.Net472))
-                .AddJob(Job.Default.WithToolchain(CsProjCoreToolchain.NetCoreApp31))
-                .AddJob(Job.Default.WithToolchain(CsProjCoreToolchain.From(new NetCoreAppSettings("net5.0-windows", null, ".NET 5.0"))))
-                ;
-
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
-        }
+        /// <summary>
+        /// usage:
+        /// dotnet run -c Release -f $moniker --filter * --runtimes net472 netcoreapp31 net5.0-windows
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
     }
 }
