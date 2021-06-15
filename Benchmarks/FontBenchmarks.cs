bb08bdd12d06812645cc1ea5e0268e96a4a2e6a4
Igor Velikorossov <igor.velikorossov@microsoft.com>
2021-06-16 08:38:11 +1000

    Add font benchmark

---
+++ Benchmarks/FontBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class FontBenchmarks
+    {
+        [Benchmark]
+        public Font CreateFont()
+        {
+            return new Font("Arial", 9f);
+        }
+
+        [Benchmark]
+        public Font SystemDefaultFont()
+        {
+            return SystemFonts.DefaultFont;
+        }
+    }
+}
